129cc6e54559efe5865152e018c42d803ff68465
Admin <mcaneha123@gmail.com>
2024-05-05 23:13:38 +0530

    Assignment04May2024updated

--- src/javaAssignment04May2024/DoubleTheArrayAssignment.java
+++ src/javaAssignment04May2024/DoubleTheArrayAssignment.java
@@ -4,7 +4,13 @@ public class DoubleTheArrayAssignment
 {
     public static void main(String[] args)
     {
-
+        int[] arr1={2,4,6,8,12,10};
+        int[] arr2=new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++)
+        {
+            arr2[i]=arr1[i]*2;
+            System.out.print(arr2[i]+", ");
+        }
 
     }
 
