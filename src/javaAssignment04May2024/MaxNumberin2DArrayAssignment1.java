7fa89f92ba55071f96d9d439d9ad8c1cd2b83456
Admin <mcaneha123@gmail.com>
2024-05-07 21:19:23 +0530

    Assignment04May2024updated

--- src/javaAssignment04May2024/MaxNumberin2DArrayAssignment1.java
+++ src/javaAssignment04May2024/MaxNumberin2DArrayAssignment1.java
@@ -4,7 +4,7 @@ public class MaxNumberin2DArrayAssignment1
 {
     public static void main(String[] args) {
         int maxNum=Integer.MIN_VALUE;
-        int[][] arr1={{1,2},{50,3},{7,10}};
+        int[][] arr1={{1, 2, 3},{4, 5, 6},{7, 8, 9}};
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j<arr1[i].length; j++)
             {
